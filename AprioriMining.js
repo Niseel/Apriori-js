f8237c9acb59c4da7bbcbf974f2deb6a8a11b05a
Niseel <nguyencongthanh200199@gmail.com>
2021-03-31 18:19:29 +0700

    add comment

--- AprioriMining.js
+++ AprioriMining.js
@@ -4,28 +4,20 @@
 
 class AprioriMining {
   static doApriori(db, supportThreshold) {
-    // Load những item có trong giao dịch
-    let I = db.getUniqueItems();
-    // Tạo biến lưu trữ các frequent k-itemset
-    let L = new ItemsetCollection(); // Resultant large itemsets
-    // Tạo biến lưu trữ các frequent tại mỗi lần thứ k duyệt
-    let Li = new ItemsetCollection(); // Large itemset in each iteration
-    // Tạo biến lưu trữ các itemset tại mỗi lần thứ k duyệt
-    let Ci = new ItemsetCollection(); // Pruned itemset in each iteration
-
+    let I = db.getUniqueItems(); // Load những item có trong giao dịch
+    let L = new ItemsetCollection(); // Tạo biến lưu trữ tất cả các frequent k-itemset
+    let Li = new ItemsetCollection(); // Tạo biến lưu trữ các frequent tại mỗi lần thứ k duyệt
+    let Ci = new ItemsetCollection(); // Tạo biến lưu trữ các itemset tại mỗi lần thứ k duyệt
     // First iteration (1-item itemsets)
     // Quét lần 1 tạo 1-itemset
     for (var i = 0; i < I.length; i += 1) {
       Ci.push(Itemset.from([I[i]]));
     }
     // Lúc này Ci = [I1 I2 I3 I4 I5]
-
-    // Next iterations
-    let k = 2;
+    let k = 2; // Biến để bắt đầu lần lặp tiếp theo
 
     // Đểm tần xuất của các 1-itemset và kiếm tra chúng với min-support
     // 1-itemset nào pass sẽ được đẩy vào Li và L
-
     while (Ci.length != 0) {
       // Set Li from Ci (pruning)
       Li.clear();
@@ -45,8 +37,7 @@ class AprioriMining {
       subsets.forEach((set) => Ci.push(set));
       k += 1;
     }
-
-    return L;
+    return L; // trả về tập các frequent k-itemset
   }
 
   static mine(db, L, confidenceThreshold) {
@@ -56,20 +47,24 @@ class AprioriMining {
     // Lặp qua hết các frequent itemset truyền vào
     for (var i in L) {
       let itemset = L[i];
-      let subsets = Bit.findSubsets(itemset, 0); // Get all subsets
+      let subsets = Bit.findSubsets(itemset, 0); // Lấy tất cả subset
 
       for (var j in subsets) {
+        // Áp dụng công thức luật X -> Y từ X -> Y-X
         let subset = subsets[j];
         let supportXY = db.findSupport(itemset);
         let supportX = db.findSupport(subset);
+        // Tính toán các chỉ số của Association Rules
         let confidence = (supportXY / supportX) * 100.0;
-        let consequent  = itemset.removeItemset(subset);
+        let consequent = itemset.removeItemset(subset);
         let supportY = db.findSupport(consequent);
         let lift = confidence / 100.0 / (supportY / 100.0);
-        let levarage = (supportXY / 100.0)  - (supportX / 100.0) * (supportY / 100.0);
+        let levarage =
+          supportXY / 100.0 - (supportX / 100.0) * (supportY / 100.0);
         let conviction = (1.0 - supportY) / (1.0 - confidence);
 
         if (confidence >= confidenceThreshold) {
+          // kiểm tra thõa mãn confidence
           let rule = new AssociationRule();
           subset.forEach((i) => rule.X.push(i));
           itemset.removeItemset(subset).forEach((i) => rule.Y.push(i));
